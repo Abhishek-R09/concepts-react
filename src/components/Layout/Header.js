4abc618e762884ceceba14a6c47603e1c5a4c3f6
Abhishek Ramasubramanian <0901abhishek.r@gmail.com>
2021-05-07 23:12:52 +0530

    Complete design, logic remaining

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+
+import classes from "./Header.module.css";
+
+import mealsImg from "../../assets/images/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="Delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
